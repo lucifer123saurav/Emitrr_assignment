f4b0e3f12c2d1d6f9b8844902aec89dcbc8eb87a
lucifer123saurav <88075350+lucifer123saurav@users.noreply.github.com>
2023-12-24 01:15:06 +0530

    Delete client directory

--- client/src/components/Error.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {ErrorComp} from "./ErrorComp"
-export const Error = ({error}) => {
-  return (
-   <ErrorComp>
-    {error}
-   </ErrorComp>
-  )
-}
--- client/src/components/ErrorComp.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const ErrorComp = styled.p`
-  color: red;
-  text-align: center;
-`;
