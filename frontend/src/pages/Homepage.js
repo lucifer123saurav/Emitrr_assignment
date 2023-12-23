2ab343491a4a2746a736e1acb7d6ad0750f7c4f7
lucifer123saurav <88075350+lucifer123saurav@users.noreply.github.com>
2023-12-24 01:20:14 +0530

    Add files via upload

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,107 @@
+import { React, useEffect } from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Center,
+  Stack,
+  Link,
+} from "@chakra-ui/react";
+import Login from "../components/authentication/Login";
+import Signup from "../components/authentication/Signup";
+import { useNavigate } from "react-router-dom";
+
+const Homepage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+    if (user) {
+      navigate("/main");
+    }
+  }, [navigate]);
+
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize={{ base: "3xl", md: "4xl" }} fontFamily="Work sans">
+          <Center>Quiz Hub</Center>
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+      <Container
+        as={Stack}
+        maxW={"6xl"}
+        mt={{ base: "10px", md: "50px" }}
+        py={4}
+        direction={{ base: "column", md: "row" }}
+        spacing={4}
+        justify={{ base: "center", md: "space-between" }}
+        align={{ base: "center", md: "center" }}
+      >
+        <Stack
+          color="white"
+          fontFamily="Work sans"
+          fontWeight="bold"
+          direction={"row"}
+          spacing={6}
+        >
+          <Link href={"https://github.com/lucifer123saurav"} target="_blank">
+            GitHub
+          </Link>
+          <Link
+            href={"https://www.linkedin.com/in/saurav-suman-b92620213/"}
+            target="_blank"
+          >
+            LinkedIn
+          </Link>
+          <Link
+            href={"https://www.instagram.com/sauravsuman92/"}
+            target="_blank"
+          >
+            Instagram
+          </Link>
+        </Stack>
+        <Text
+          color="white"
+          fontFamily="Work sans"
+          fontWeight="bold"
+          fontSize="16px"
+        >
+          Made with ❤️ by Saurav Suman
+        </Text>
+      </Container>
+    </Container>
+  );
+};
+
+export default Homepage;
